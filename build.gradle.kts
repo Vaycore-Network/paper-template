9ff4f4b20d0e91eebee6f931ee8843a5da88dffc
c4vxl <manage@c4vxl.de>
2026-03-08 11:14:27 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+}
+
+group = "de.c4vxl"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-public/")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    // Paper API
+    paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
+
+    // Command API
+    implementation("dev.jorel:commandapi-paper-shade:11.1.0")
+    implementation("dev.jorel:commandapi-kotlin-paper:11.1.0")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.assemble {
+    dependsOn(tasks.reobfJar)
+}
+
+// Mojang mapped
+paperweight {
+    reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
+}
+
+// Plugin config
+bukkit {
+    name = "paper-template"
+    description = "This a template plugin"
+    main = "$group.papertemplate.Main"
+    version = "1.0.0"
+    apiVersion = "1.14" // 1.14+
+
+    authors = listOf("c4vxl")
+    website = "https://c4vxl.de/"
+
+    libraries = listOf(
+        "org.jetbrains.kotlin:kotlin-stdlib:2.1.10",
+        "dev.jorel:commandapi-paper-shade:11.1.0",
+        "dev.jorel:commandapi-kotlin-paper:11.1.0"
+    )
+}
---
+++ src/main/kotlin/de/c4vxl/papertemplate/Main.kt
@@ -0,0 +1,39 @@
+package de.c4vxl.papertemplate
+
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIPaperConfig
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Logger
+
+class Main : JavaPlugin() {
+    companion object {
+        lateinit var instance: Main
+        lateinit var logger: Logger
+    }
+
+    override fun onLoad() {
+        instance = this
+        Main.logger = this.logger
+
+        // Load CommandAPI
+        CommandAPI.onLoad(
+            CommandAPIPaperConfig(this)
+                .silentLogs(true)
+                .verboseOutput(false)
+        )
+    }
+
+    override fun onEnable() {
+        // Enable CommandAPI
+        CommandAPI.onEnable()
+
+        logger.info("[+] $name has been enabled!")
+    }
+
+    override fun onDisable() {
+        // Disable CommandAPI
+        CommandAPI.onDisable()
+
+        logger.info("[+] $name has been disabled!")
+    }
+}
